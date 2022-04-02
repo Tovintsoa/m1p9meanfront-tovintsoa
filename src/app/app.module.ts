fb3bcdb761c84e2e6099b4866092f821803e5505
Tovintsoa <tianatovintsoa@gmail.com>
2022-04-02 23:03:23 +0300

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
@@ -35,7 +35,8 @@ const routes: Routes = [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    NgxSpinnerModule
+    NgxSpinnerModule,
+    ReactiveFormsModule
   ],
   providers: [authInterceptorProviders],
   bootstrap: [AppComponent]
--- src/app/inscription/inscription.component.ts
+++ src/app/inscription/inscription.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {UserService} from "../service/user.service";
 import {Restaurant} from "../model/restaurant.model";
+import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import Validation from "../_helpers/Validation";
 
 @Component({
   selector: 'app-inscription',
@@ -8,6 +10,14 @@ import {Restaurant} from "../model/restaurant.model";
   styleUrls: ['./inscription.component.css']
 })
 export class InscriptionComponent implements OnInit {
+  form: FormGroup = new FormGroup({
+    nom: new FormControl(''),
+    prenom: new FormControl(''),
+    email: new FormControl(''),
+    motdepasse: new FormControl(''),
+    motdepasse2: new FormControl(''),
+    role:new FormControl(''),
+  });
   user: Restaurant = {
     nom: '',
     prenom:'',
@@ -17,11 +27,43 @@ export class InscriptionComponent implements OnInit {
     url_image:'',
   };
   submitted = false;
-  constructor(private  userService:UserService) { }
+  constructor(private  userService:UserService,private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group(
+      {
+        nom: ['', Validators.required],
+        prenom: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(20)
+          ]
+        ],
+        email: ['', [Validators.required, Validators.email]],
+        motdepasse: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(40)
+          ]
+        ],
+        motdepasse2: ['', Validators.required],
+        role:['',Validators.required]
+      },
+      {
+        validators: [Validation.match('motdepasse', 'motdepasse2')]
+      },
+
+    );
+  }
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
   }
   saveUser():void{
+    this.submitted = true;
     const data = {
       nom:this.user.nom,
       prenom:this.user.prenom,
@@ -29,15 +71,19 @@ export class InscriptionComponent implements OnInit {
       role:this.user.role,
       description:this.user.description,
       url_image:this.user.url_image,
-      motdepasse: this.user.motdepasse
+      motdepasse: this.user.motdepasse,
     };
     this.userService.create(data).subscribe(
       response => {
         console.log(response);
-        this.submitted = true;
+
       },
       error => {
         console.log(error);
       });
+  }
+  onReset(): void {
+    this.submitted = false;
+
   }
 }
