6aafeb4ebb4436474d3aa00a4a33738c93b8a423
Tovintsoa <tianatovintsoa@gmail.com>
2022-04-09 18:21:22 +0300

    Push panier

--- src/app/panier/panier.component.ts
+++ src/app/panier/panier.component.ts
@@ -10,25 +10,55 @@ import {TokenStorageService} from "../service/token-storage.service";
 })
 export class PanierComponent implements OnInit {
   panierListe! : any;
-  user ;
-  constructor(panierService:PanierService,private tokenStorage: TokenStorageService) {
-    this.user = tokenStorage.getUser();
+  user! : any ;
+  total = 0 ;
+  constructor(private panierService:PanierService,private tokenStorage: TokenStorageService) {
+
+  }
+
+  ngOnInit(): void {
+    this.user = this.tokenStorage.getUser();
     if(null === this.tokenStorage.getToken()) {
       this.panierListe = {};
     }
     else {
-      this.panierListe = panierService.getListePanier(tokenStorage.getUser().id).subscribe({
+      this.panierListe = this.panierService.getListePanier(this.tokenStorage.getUser().id).subscribe({
         next: (data) => {
           this.panierListe = data;
-          console.log(data);
-          /*this.panier = data;*/
+          this.calculTotal();
         },
         error: (e) => console.error(e)
       })
     }
   }
-
-  ngOnInit(): void {
+  effacerPanier(idPanier:string){
+      let idUser = this.tokenStorage.getUser().id;
+      this.panierListe = this.panierService.effacerPanier(idPanier,idUser).subscribe({
+        next: (data) => {
+          this.panierListe = data;
+          this.calculTotal();
+        },
+        error: (e) => console.error(e)
+      });
   }
+  changeQuantite(idPanier:string,event:any){
+    let idUser = this.tokenStorage.getUser().id;
+    let qte = event.target.value;
+    this.panierService.changeQuantite(idPanier,qte,idUser).subscribe({
+      next: (data) => {
+        this.panierListe = data;
+        this.calculTotal();
+      },
+      error: (e) => console.error(e)
+    });
+  }
+  calculTotal(){
+    console.log(this.panierListe);
+    this.total = 0;
+    for(let j=0;j<this.panierListe.length;j++){
+      let temp = this.panierListe[j].plat.prix * this.panierListe[j].quantite;
 
+      this.total+= temp;
+    }
+  }
 }
--- src/app/service/panier.service.ts
+++ src/app/service/panier.service.ts
@@ -20,4 +20,10 @@ export class PanierService {
   ajouterPanier(data: any): Observable<any>{
     return this.http.post(baseUrl+"/api/ajouterPanier", data)
   }
+  effacerPanier(idPanier:string,idUser: string){
+    return this.http.get(baseUrl+"/api/deletePanier/"+idPanier+'/'+idUser);
+  }
+  changeQuantite(idPanier:string,qte:number,idUser: string){
+    return this.http.get(baseUrl+"/api/panier/changeQuantite/"+idPanier+"/"+qte+"/"+idUser);
+  }
 }
